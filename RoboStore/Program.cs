400409bf43cb367e00c0458fae481b5644cf2b6f
ImanKris <ImanKris@mail.ru>
2026-03-30 17:55:29 +0300

    это пароль

--- RoboStore/Program.cs
+++ RoboStore/Program.cs
@@ -21,3 +21,8 @@ app.UseRouting();
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+});
+app.UseSession();
