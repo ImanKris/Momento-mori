bb66f659365908586fe4409e17faf29acb78ca9e
ImanKris <ImanKris@mail.ru>
2026-03-30 21:54:47 +0300

    нет, вот на этом остановились

--- RoboStore/obj/Debug/net8.0/RoboStore.AssemblyInfo.cs
+++ RoboStore/obj/Debug/net8.0/RoboStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoboStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f44e200db447717ca2ad2f1a3f89f09b59dfc8a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+536d8715ff0ba409a7cea2c820e32cf5dd41f709")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoboStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoboStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
