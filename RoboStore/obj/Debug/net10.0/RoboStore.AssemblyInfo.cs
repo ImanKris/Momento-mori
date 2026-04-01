2b5f2ddd3956424db87e2574c74f0e8eba127229
ImanKris <ImanKris@mail.ru>
2026-04-01 16:31:29 +0300

    ну коммитниксь ты уже нормально

--- RoboStore/obj/Debug/net10.0/RoboStore.AssemblyInfo.cs
+++ RoboStore/obj/Debug/net10.0/RoboStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoboStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d1102c0f5e65045ece84c7087599b730b0a705a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5e94b27889777591fc1ea5605d839f4f9184dd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoboStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoboStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
