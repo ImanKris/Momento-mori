f6558421a947ea8ade2f123650f3a7ba915d5d7e
Иван Иванов <momentomori41@outlook.com>
2026-04-04 12:40:02 +0300

    хз что это

--- RoboStoreBot/obj/Debug/net8.0/RoboStoreBot.AssemblyInfo.cs
+++ RoboStoreBot/obj/Debug/net8.0/RoboStoreBot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoboStoreBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+742e9b66e07daee68f3275c768a71f526984bbb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88f645c4f52b276ddd4ddb2c8907d321deb91cbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoboStoreBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoboStoreBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
